8e54093ccd70a39ed5d0aa09f368071f732273f9
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-06-06 12:00:10 +0530

    Update config_var.py

--- main_startup/config_var.py
+++ main_startup/config_var.py
@@ -43,7 +43,7 @@ class Config(object):
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     STRINGSESSION = os.environ.get("STRINGSESSION", None)
     ASSISTANT_START_PIC = os.environ.get(
-        "ASSISTANT_START_PIC", "https://telegra.ph//file/92c1a600394c723db90fc.jpg"
+        "ASSISTANT_START_PIC", "https://telegra.ph//file/5f9ddf0e3a3859a4bb288.jpg"
     )
     STRINGSESSION_2 = os.environ.get("STRINGSESSION_2", None)
     STRINGSESSION_3 = os.environ.get("STRINGSESSION_3", None)
@@ -65,7 +65,7 @@ class Config(object):
         "UPSTREAM_REPO", "https://github.com/Mr-SHRLCK/FD-UB"
     )
     ALIVE_IMG = os.environ.get(
-        "ALIVE_IMG", "https://telegra.ph//file/eb33d2acd623f061b1809.jpg"
+        "ALIVE_IMG", "https://telegra.ph//file/5f9ddf0e3a3859a4bb288.jpg"
     )
     U_BRANCH = "master"
     HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
