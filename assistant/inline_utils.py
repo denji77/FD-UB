c84fdd44f31cf5cddaf343dd197d1278e3b4c681
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-06-07 09:07:10 +0530

    Update inline_utils.py

--- assistant/inline_utils.py
+++ assistant/inline_utils.py
@@ -323,10 +323,10 @@ async def black_menu(client, cb):
     total_ = len(CMD_LIST)
     bttn = [
         [
-                InlineKeyboardButton(
-                    text="Command - Help", callback_data=f"make_cmd_buttons"),
-                InlineKeyboardButton(
-                    text="Restart - Userbot", callback_data=f"restart_bot),
+             InlineKeyboardButton(
+                   text="Command - Help", callback_data=f"make_cmd_buttons"),
+             InlineKeyboardButton(
+                   text="Restart - Userbot", callback_data=f"restart_bot),
             ],
             [
              InlineKeyboardButton(
